33f276db18584818f545734f64d283c4098da5e3
tujianglin <jianglin.tu@wimisoft.com>
2023-08-14 17:39:56 +0800

    feat: 日志完成

--- src/logs/logs.module.ts
+++ src/logs/logs.module.ts
@@ -1,7 +1,45 @@
 import { Module } from '@nestjs/common';
 import { LogsService } from './logs.service';
 import { LogsController } from './logs.controller';
+import { WinstonModule, utilities } from 'nest-winston';
+import * as winston from 'winston';
+
+function createDailyRotateFile(level: string, filename: string) {
+  return new winston.transports.DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
 @Module({
+  imports: [
+    WinstonModule.forRootAsync({
+      useFactory: () => {
+        return {
+          transports: [
+            new winston.transports.Console({
+              level: 'info',
+              format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.ms(),
+                utilities.format.nestLike('TEST'),
+              ),
+            }),
+            createDailyRotateFile('info', 'application'),
+            createDailyRotateFile('warn', 'error'),
+          ],
+        };
+      },
+    }),
+  ],
   controllers: [LogsController],
   providers: [LogsService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { WinstonModule, utilities } from 'nest-winston';
-import * as winston from 'winston';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import 'winston-daily-rotate-file';
-import { DailyRotateFile } from 'winston/lib/winston/transports';
-function createDailyRotateTrasnport(level: string, filename: string) {
-  return new DailyRotateFile({
-    level,
-    dirname: 'logs',
-    filename: `${filename}-%DATE%.log`,
-    datePattern: 'YYYY-MM-DD-HH',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d',
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.simple(),
-    ),
-  });
-}
 async function bootstrap() {
-  const instance = winston.createLogger({
-    transports: [
-      new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.ms(),
-          utilities.format.nestLike('TEST', {
-            colors: true,
-            prettyPrint: true,
-          }),
-        ),
-      }),
-      createDailyRotateTrasnport('info', 'application'),
-      createDailyRotateTrasnport('warn', 'error'),
-    ],
-  });
-  const app = await NestFactory.create(AppModule, {
-    // winston 替换 nest 日志
-    logger: WinstonModule.createLogger({
-      instance,
-    }),
-  });
+  const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   /* swigger配置 */
   const config = new DocumentBuilder()
     .setTitle('接口')
